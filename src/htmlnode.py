1e1b6c493f0fcd635b131d63da8bd45f8397b088
myticalCat <spongedraw@gmail.com>
2026-04-22 17:10:48 +0700

    add LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,6 +12,8 @@ class HTMLNode():
     def props_to_html(self) -> str:
         html_str = ""
 
+        if self.props is None:
+            return html_str
         for k, v in self.props.items():
             html_str += f' {k}="{v}"'
             
@@ -25,3 +27,29 @@ class HTMLNode():
             "props" : self.props
         }.__repr__()
 
+
+class LeafNode(HTMLNode):
+
+    def __init__(self, tag, value, props = None):
+        super().__init__(
+            tag=tag,
+            value=value,
+            children=None,
+            props=props
+        )
+
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return {
+            "tag" : self.tag,
+            "value" : self.value,
+            "props" : self.props
+        }.__repr__()
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestTextNode(unittest.TestCase):
@@ -36,6 +36,23 @@ class TestTextNode(unittest.TestCase):
         
         self.assertNotEqual(node, node2)
 
+    def test_props_none(self): 
+        self.assertEqual(HTMLNode(props=None).props_to_html(), "")
+
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Hello, world!", {
+            "href" : "mytical.cat"
+        })
+        self.assertEqual(node.to_html(), '<a href="mytical.cat">Hello, world!</a>')
+
+    def test_leaf_to_html_no_value(self):
+        invalid = LeafNode("p", None, {"a":"cat.com"})
+        self.assertRaises(ValueError, invalid.to_html)
 
 if __name__ == "__main__":
     unittest.main()
