1661d5b62a2354151068337e97e6b2b31617cdfa
myticalCat <spongedraw@gmail.com>
2026-05-03 00:02:29 +0700

    add block to block type

--- src/blocknode.py
+++ src/blocknode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 
 class BlockType(Enum):
     PAR = "paragraph"
@@ -7,3 +8,23 @@ class BlockType(Enum):
     CODE = "code"
     U_LIST = "unordered_list"
     O_LIST = "ordered_list"
+
+def block_to_block_type(block : str) -> BlockType:
+    heading_prefix = [(i * "#")+" " for i in range(1, 7)]
+    for sw in heading_prefix:
+        if block.startswith(sw):
+            return BlockType.HEAD
+    
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    if block.startswith(">"):
+        return BlockType.QUOTE
+    
+    if block.startswith("-"):
+        return BlockType.U_LIST
+    
+    if re.findall(r"^[0-9]*\. "):
+        return BlockType.O_LIST
+    
+    return BlockType.PAR
