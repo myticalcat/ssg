8d746deb3626f3e713912954cd7d08c1258d8079
myticalCat <spongedraw@gmail.com>
2026-05-02 23:00:17 +0700

    add test for image splitter

--- src/test_helper.py
+++ src/test_helper.py
@@ -1,6 +1,6 @@
 import unittest
 
-from function_helper import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from function_helper import text_node_to_html_node, split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
 
 from textnode import TextNode, TextType
 from htmlnode import LeafNode
@@ -88,5 +88,23 @@ class TestText2HTML(unittest.TestCase):
         text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
         self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], extract_markdown_links(text))
 
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
 if __name__ == "__main__":
     unittest.main()
