eaf06e21c423166899b87ec5004920da44bcb45d
myticalCat <spongedraw@gmail.com>
2026-05-04 16:07:19 +0700

    add scaffolding to move static files to public

--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+def copy_static():
+    if not os.path.exists("./static"):
+        print("no static path dectected!")
+        return
+    if not os.path.exists("./public"):
+        print("no public path dectected!")
+        return
+    copy_recusive("./static", "./public", True)
+
+def copy_recusive(from_path, to_path, is_root = False):
+    print(os.listdir(from_path))
+
+
 if __name__ == "__main__":
-    print(TextNode("hello",TextType.LINK, "mytical.cat"))
+    copy_static()
+    # print(TextNode("hello",TextType.LINK, "mytical.cat"))
